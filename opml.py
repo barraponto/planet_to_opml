266bac28059bd6cc1ceedbb1dbe3c57cc14a4947
Capi Etheriel <barraponto@gmail.com>
2012-01-29 23:58:49 +0000

    converted opml code to OO

--- opml.py
+++ opml.py
@@ -2,17 +2,24 @@
 from lxml import etree
 from scrape import feeds
 
-root = etree.Element('opml')
-root.set('version', '2.0')
-head = etree.SubElement(root, 'head')
-body = etree.SubElement(root, 'body')
-for feed in feeds:
-    outline = etree.SubElement(body, 'outline')
-    outline.set('text', feed['title'])
-    outline.set('type', 'rss')
-    outline.set('htmlUrl', feed['section'])
-    outline.set('xmlUrl', feed['feed'])
+class OPML:
+    def __init__(self, title=False, outlines=[]):
+        self.root = etree.Element('opml', version='2.0')
+        self.head = etree.SubElement(self.root, 'head')
+        self.body = etree.SubElement(self.root, 'body')
+        if isinstance(title, basestring):
+            self.title = title
+            etree.SubElement(self.head, 'title').text = self.title
+        for outline in outlines:
+            etree.SubElement(
+                    self.body, 'outline', text=outline['text'], type='rss',
+                    htmlUrl=outline['htmlUrl'], xmlUrl=outline['xmlUrl'])
 
-opml = open('drupal-planet.opml', 'w')
-opml.write(etree.tostring(root, pretty_print=True, encoding='utf-8'))
 
+def main():
+    opml = OPML('Drupal Planet OPML', feeds)
+    file = open('drupal-planet.opml', 'w')
+    file.write(etree.tostring(opml.root, pretty_print=True, encoding='utf-8'))
+
+if __name__ == '__main__':
+    main()
