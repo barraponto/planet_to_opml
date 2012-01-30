0b422f9e299aa0193024dfd38d61a10735562ccf
Capi Etheriel <barraponto@gmail.com>
2012-01-30 22:19:56 +0000

    moved scraper code to main()

--- drupal.py
+++ drupal.py
@@ -1,21 +1,19 @@
 # -*- coding: utf-8 -*-
-from lxml.html import parse
 from urlparse import urlparse
+from lxml.html import parse
+from lxml import etree
 from opml import OPML
 
-feeds = []
-dom = parse('http://drupal.org/planet').getroot()
-
-for line in dom.cssselect('#block-drupalorg_news-planet-list .item-list li'):
-    links = line.cssselect('a')
-    feeds.append({
-        'text': links[0].text_content(),
-        'htmlUrl': links[0].get('href'), 
-        'xmlUrl': links[1].get('href'),
-    })
-
 def main():
-    opml = OPML('Drupal Planet OPML', feeds)
+    opml = OPML('Drupal Planet OPML')
+    dom = parse('http://drupal.org/planet').getroot()
+    for line in dom.cssselect('#block-drupalorg_news-planet-list .item-list li'):
+        links = line.cssselect('a')
+        opml.add_rss(
+                text=links[0].text_content(),
+                htmlUrl=links[0].get('href'), 
+                xmlUrl=links[1].get('href'),
+                )
     file = open('drupal-planet.opml', 'w')
     file.write(etree.tostring(opml.root, pretty_print=True, encoding='utf-8'))
 
