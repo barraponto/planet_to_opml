93530898089a8297f6b4931aba45353b554a5b49
Capi Etheriel <barraponto@gmail.com>
2012-01-30 22:49:39 +0000

    added scrape method to DrupalPlanetOPML subclass, revamped main()

--- drupal.py
+++ drupal.py
@@ -1,21 +1,22 @@
 # -*- coding: utf-8 -*-
-from urlparse import urlparse
 from lxml.html import parse
-from lxml import etree
 from opml import OPML
 
+class DrupalPlanetOPML(OPML):
+    def scrape(self, url):
+        dom = parse(url).getroot()
+        for line in dom.cssselect('#block-drupalorg_news-planet-list .item-list li'):
+            links = line.cssselect('a')
+            self.add_rss(
+                    text=links[0].text_content(),
+                    htmlUrl=links[0].get('href'), 
+                    xmlUrl=links[1].get('href'),
+                    )
+
 def main():
-    opml = OPML('Drupal Planet OPML')
-    dom = parse('http://drupal.org/planet').getroot()
-    for line in dom.cssselect('#block-drupalorg_news-planet-list .item-list li'):
-        links = line.cssselect('a')
-        opml.add_rss(
-                text=links[0].text_content(),
-                htmlUrl=links[0].get('href'), 
-                xmlUrl=links[1].get('href'),
-                )
-    file = open('drupal-planet.opml', 'w')
-    file.write(etree.tostring(opml.root, pretty_print=True, encoding='utf-8'))
+    opml = DrupalPlanetOPML('Drupal Planet OPML')
+    opml.scrape('http://drupal.org/planet')
+    opml.write_file('drupal-planet.opml')
 
 if __name__ == '__main__':
     main()
